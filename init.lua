b1fc7b870b00b6abc19a29ae5c2b156e2d9ceed7
Rasmus Bertell <rasmus@bertell.fi>
2022-03-10 11:51:25 +0200

    feat(indent): template indent size for easy config

--- init.lua
+++ init.lua
@@ -12,9 +12,9 @@ vim.opt.number         = true
 vim.opt.relativenumber = true
 
 -- Indentation
-vim.opt.tabstop     = 4
-vim.opt.shiftwidth  = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop     = {{nvim.indent.size}}
+vim.opt.shiftwidth  = {{nvim.indent.size}}
+vim.opt.softtabstop = {{nvim.indent.size}}
 vim.opt.expandtab   = true
 vim.opt.smarttab    = true
 vim.opt.autoindent  = true
