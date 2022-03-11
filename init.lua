a6547a7b842faebcbc5a55a79a54ae2aca0854b1
RasmusBertell <rasmus@bertell.fi>
2022-03-11 18:18:07 +0200

    Remove unnecessary configs

--- init.lua
+++ init.lua
@@ -2,10 +2,6 @@
 vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader   = " "
 
--- Testing keymaps
-vim.api.nvim_set_keymap("n", "<leader>n", ":bnext<cr>",     { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>p", ":bprevious<cr>", { noremap = true })
-
 -- Line numbers
 vim.opt.number         = true
 vim.opt.relativenumber = true
@@ -14,22 +10,14 @@ vim.opt.relativenumber = true
 vim.opt.tabstop     = {{ default.indent.size }}
 vim.opt.shiftwidth  = {{ default.indent.size }}
 vim.opt.softtabstop = {{ default.indent.size }}
-vim.opt.expandtab   = true
-vim.opt.smarttab    = true
-vim.opt.autoindent  = true
+vim.opt.expandtab   = {{ default.indent.spaces }}
 vim.opt.smartindent = true
 
 -- Folding
 vim.opt.foldmethod = "syntax"
 vim.g.php_folding  = 2
 
--- Statusbar
-vim.opt.showmode    = false
-vim.opt.showcmd     = false
-vim.opt.ruler       = false
-vim.opt.laststatus  = 2
-
-vim.opt.scrolloff   = 100
+vim.opt.scrolloff   = 25
 vim.opt.colorcolumn = { 80, 100, 120 }
 
 require("plugins")
