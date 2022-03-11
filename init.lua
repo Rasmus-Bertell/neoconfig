eeeb98d4b1398fbd9a8b3cf23535c4de9a537374
RasmusBertell <rasmus@bertell.fi>
2022-03-11 17:43:04 +0200

    Cleanup config

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 -- Options
-vim.opt.clipboard     = "unnamedplus"
-vim.g.mapleader       = " "
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader   = " "
 
 -- Testing keymaps
-vim.api.nvim_set_keymap("n", "<leader><esc>", ":nohlsearch<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>n",     ":bnext<cr>",      { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>p",     ":bprevious<cr>",  { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>n", ":bnext<cr>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>p", ":bprevious<cr>", { noremap = true })
 
 -- Line numbers
 vim.opt.number         = true
@@ -20,17 +19,17 @@ vim.opt.smarttab    = true
 vim.opt.autoindent  = true
 vim.opt.smartindent = true
 
+-- Folding
 vim.opt.foldmethod = "syntax"
 vim.g.php_folding  = 2
 
-vim.opt.completeopt = "menu,menuone,noselect"
-
+-- Statusbar
 vim.opt.showmode    = false
 vim.opt.showcmd     = false
 vim.opt.ruler       = false
 vim.opt.laststatus  = 2
 
-vim.opt.scrolloff   = 25
-vim.opt.colorcolumn = { 80 }
+vim.opt.scrolloff   = 100
+vim.opt.colorcolumn = { 80, 100, 120 }
 
 require("plugins")
