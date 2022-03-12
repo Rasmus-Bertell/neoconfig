5ef0f2c49a8bd965a0f077187957ebff4e016fcb
RasmusBertell <rasmus@bertell.fi>
2022-03-12 12:23:51 +0200

    feat(kanagawa): remove setup in favor of defaults

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,22 +1,4 @@
 local function config()
-    require("kanagawa").setup(
-        {
-            undercurl            = true,
-            commentStyle         = "italic",
-            functionStyle        = "NONE",
-            keywordStyle         = "italic",
-            statementStyle       = "bold",
-            typeStyle            = "NONE",
-            variablebuiltinStyle = "italic",
-            specialReturn        = true,
-            specialException     = true,
-            transparent          = false,
-            dimInactive          = true,
-            colors               = {},
-            overrides            = {},
-        }
-    )
-
     vim.opt.termguicolors = true
     vim.cmd("colorscheme kanagawa")
 end
