36c00e05718eee0078f23247ef367cfc576f4551
RasmusBertell <rasmus@bertell.fi>
2022-03-14 17:45:22 +0200

    feat(heirline): separate ruler and scroll to modules

---
+++ lua/plugins/heirline/file/ruler.lua
@@ -0,0 +1,3 @@
+return {
+    provider = "%7(%l/%3L%):%2c %P",
+}
--- lua/plugins/heirline/init.lua
+++ lua/plugins/heirline/init.lua
@@ -1,37 +1,20 @@
 local function config()
-    -- local conditions = require("heirline.conditions")
     local utils      = require("heirline.utils")
     local kanagawa   = require("kanagawa.colors").setup()
 
     local Align = { provider = "%=" }
     local Space = { provider = " " }
 
-    local mode = require("plugins.heirline.mode")
-    local file = require("plugins.heirline.file.block")
-
+    local mode       = require("plugins.heirline.mode")
+    local file       = require("plugins.heirline.file.block")
     local fileType   = require("plugins.heirline.file.type")
     local fileFormat = require("plugins.heirline.file.format")
-
-    local Ruler = {
-        provider = "%7(%l/%3L%):%2c %P",
-    }
-
-    local ScrollBar ={
-        static = {
-            sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' }
-        },
-        provider = function(self)
-            local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-            local lines = vim.api.nvim_buf_line_count(0)
-            local i = math.floor(curr_line / lines * (#self.sbar - 1)) + 1
-
-            return self.sbar[i]
-        end
-    }
+    local ruler      = require("plugins.heirline.file.ruler")
+    local scroll     = require("plugins.heirline.file.scroll")
 
     Left   = utils.surround({ "", "" }, kanagawa.waveBlue1, { mode })
     Middle = utils.surround({ "", "" }, kanagawa.waveBlue1, { file })
-    Right  = { fileType, Space, fileFormat, Space, Ruler, Space, ScrollBar }
+    Right  = { fileType, Space, fileFormat, Space, ruler, Space, scroll }
 
     require("heirline").setup({ Left, Align, Middle, Align, Right });
 end
