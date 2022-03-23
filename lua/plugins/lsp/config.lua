17da694acdd9262d3c8ee9f2a6a743dfba13df44
RasmusBertell <rasmus@bertell.fi>
2022-03-23 17:49:18 +0200

    feat(lsp): add config for sumneko_lua

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -3,6 +3,33 @@ local function config()
 
     lsp_installer.on_server_ready(function(server)
         local opts = {}
+
+        if server.name == "sumneko_lua" then
+            local runtime_path = vim.split(package.path, ';')
+            table.insert(runtime_path, "lua/?.lua")
+            table.insert(runtime_path, "lua/?/init.lua")
+
+            opts = {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                            path    = runtime_path,
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            }
+        end
+
         server:setup(opts)
     end)
 end
