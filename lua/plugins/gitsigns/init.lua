f13b2b674a59b207048ae004d055f320754cde45
RasmusBertell <rasmus@bertell.fi>
2022-05-26 20:42:58 +0300

    feat: add initial gitsigns keymap

--- lua/plugins/gitsigns/init.lua
+++ lua/plugins/gitsigns/init.lua
@@ -1,5 +1,8 @@
 local config = function()
+        local keymap = require("plugins.gitsigns.keymap")
+
         require("gitsigns").setup({
+                on_attach = keymap,
                 current_line_blame = true,
                 current_line_blame_opts = {
                         virt_text_pos = 'eol',
